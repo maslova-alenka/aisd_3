beaf886750bc0f0d8231e3314b0d0ddf00a8e586
Светлана Маслова <svetlana111019@outlook.com>
2023-12-03 23:58:40 +0400

    add  quick_sort

--- include/Sort.h
+++ include/Sort.h
@@ -26,5 +26,53 @@ Stats insertion_sort(std::vector<T>& arr) {
 }
 
 
+// быстрая сортировка 
+struct Partition {
+    int start;
+    int end;
+};
+
+Stats quick_sort(std::vector<int>& arr, int start, int end) {
+    Stats stats;
+    std::vector<Partition> partitions;
+    partitions.push_back({ start, end });
+
+    while (!partitions.empty()) {
+        start = partitions.back().start;
+        end = partitions.back().end;
+        partitions.pop_back();
+
+        if (start < end) {
+            int pivot = arr[end];
+            int i = start - 1;
+
+            for (int j = start; j <= end - 1; ++j) {
+                stats.comparison_count++;
+                if (arr[j] < pivot) {
+                    ++i;
+                    std::swap(arr[i], arr[j]);
+                    stats.copy_count++;
+                }
+            }
+
+            std::swap(arr[i + 1], arr[end]);
+            stats.copy_count++;
+
+            int partition_index = i + 1;
+
+            partitions.push_back({ start, partition_index - 1 });
+            partitions.push_back({ partition_index + 1, end });
+        }
+    }
+
+    return stats;
+}
+
+Stats quick_sort_wrapper(std::vector<int>& arr) {
+    return quick_sort(arr, 0, arr.size() - 1);
+}
+
+// пирамидальная сортировка
+
 
 #endif
--- src/main.cc
+++ src/main.cc
@@ -1,1 +1,25 @@
 #include "../include/Sort.h"
+
+template<typename T>
+void print_arr(std::vector<T> arr) {
+	for (T num : arr) {
+		std::cout << num << ' ';
+	}
+}
+
+
+int main() {
+	std::vector<int> arr{ 1, 4, 7, 3, 9, 8, 5, 6, 2 };
+	print_arr(arr);
+	std::cout << std::endl;
+	std::cout << insertion_sort(arr);
+	print_arr(arr);
+	std::cout << std::endl;
+
+
+	std::vector<int> data = { 1, 4, 7, 3, 9, 8, 5, 6, 2 };
+	print_arr(data);
+	std::cout << std::endl;
+	std::cout << quick_sort_wrapper(data);
+	print_arr(data);
+}
